5d951051b0089485ae1c33182b93f470b4dc5c71
yo1ch1 <dmit.petukhov2017@gmail.com>
2024-08-23 00:18:18 +1200

    Add files via upload

---
+++ moduel_1_5.py
@@ -0,0 +1,6 @@
+inmutable_var = (1, 'two', True)
+print(inmutable_var) #Кортеж не поддерживает обращение по элементам
+mutable_list = ['tomato', 'carrot', 'potato']
+print(mutable_list)
+mutable_list [1] ='pineapple'
+print(mutable_list)
